d2af3a732785e2c2acd67a2da47f239c55e5a4f2
MitchelMA <vonkm124@gmail.com>
2023-03-17 13:33:28 +0100

    meer in readme.md

--- Lexer/CsvSettings.cs
+++ Lexer/CsvSettings.cs
@@ -2,12 +2,14 @@
 
 public struct CsvSettings
 {
+    public bool FirstIsHeader;
     public char Separator;
     public char? CommentStarter;
     public bool Patches;
 
     public static CsvSettings Default => new()
     {
+        FirstIsHeader = false,
         Separator = ',',
         CommentStarter = null,
         Patches = false,
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Csv Lexing
 Hier doe ik een poging tot het maken van een simpele csv-lexer die voldoet aan overgrote deel van de specs.
+
+## Features
+- [x] comment-support
+- [ ] full string-support
+- [ ] negeren van trailing space-chars e.g "John&emsp;,&emsp;Doe" ->  "John" en "Doe"
+- [ ] Embedded line-breaks middels comma's
