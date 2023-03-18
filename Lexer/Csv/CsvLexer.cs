814c76ed275d0326bcd7412687a7a957a31d164b
MitchelMA <vonkm124@gmail.com>
2023-03-18 13:42:13 +0100

    liner verplaatst en begin aan string-handling

--- Lexer/Csv/CsvLexer.cs
+++ Lexer/Csv/CsvLexer.cs
@@ -8,12 +8,6 @@ public class CsvLexer
     private readonly FileInfo _file;
     private readonly CsvSettings _settings = CsvSettings.Default;
 
-    private delegate bool ModusDelegate(ref string buffer, ICollection<string> lines, Stream s);
-
-    private Dictionary<PreLexModus, ModusDelegate> _preLexMethods = new();
-    private PreLexModus _currentModus = PreLexModus.Default;
-
-
     public CsvLexer(string filePath)
     {
         _file = new FileInfo(PathHelper.ToAbsoluteDomain(filePath));
@@ -22,7 +16,6 @@ public class CsvLexer
             throw new FileNotFoundException($"File {_file.FullName} does not exist");
         }
 
-        PopulateDict();
     }
 
     public CsvLexer(string filepath, CsvSettings settings)
@@ -34,127 +27,12 @@ public class CsvLexer
         }
 
         _settings = settings;
-        PopulateDict();
     }
-
-    private void PopulateDict()
-    {
-        _preLexMethods.Add(PreLexModus.Default, DefaultHandler);
-        _preLexMethods.Add(PreLexModus.String, StringHandler);
-        _preLexMethods.Add(PreLexModus.Comment, CommentHandler);
-    }
-
-    #region PreLexing
-
-    /// <summary>
-    /// The first iteration: going through the file, and parsing out the comments if enabled and
-    /// parsing the lines.
-    /// </summary>
-    /// <param name="fs"></param>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
-    private string[] GetLines(FileStream fs)
-    {
-        string buffer = string.Empty;
-        List<string> lines = new();
-        bool available = true;
-
-        while (available)
-        {
-            available = _preLexMethods[_currentModus](ref buffer, lines, fs);
-        }
-
-        return lines.ToArray();
-    }
-
-    private bool _EOLproc(ref string buffer, ICollection<string> lines)
-    {
-        string trimmed = buffer.Trim();
-        if (_settings.Patches && trimmed.Length == 0)
-            return false;
-
-        lines.Add(trimmed);
-        buffer = "";
-        return true;
-    }
-
-    private bool DefaultHandler(ref string buffer, ICollection<string> lines, Stream stream)
-    {
-        int c;
-
-        while ((c = stream.ReadByte()) != -1)
-        {
-            if (ToCommentModus(c))
-            {
-                _currentModus = PreLexModus.Comment;
-                return true;
-            }
-
-            if (ToStringModus(c))
-            {
-                _currentModus = PreLexModus.String;
-                return true;
-            }
-
-
-            if (c == '\n')
-            {
-                _EOLproc(ref buffer, lines);
-                continue;
-            }
-
-            buffer += (char)c;
-        }
-
-        _EOLproc(ref buffer, lines);
-
-        return false;
-    }
-
-    private bool StringHandler(ref string buffer, ICollection<string> lines, Stream stream)
-    {
-        throw new NotImplementedException();
-    }
-
-    private bool CommentHandler(ref string buffer, ICollection<string> lines, Stream stream)
-    {
-        // keep consuming till the end of the line
-        int c;
-        while ((c = stream.ReadByte()) is not '\n' and not -1) ;
-        switch (c)
-        {
-            case '\n':
-                _EOLproc(ref buffer, lines);
-                break;
-            case -1:
-                _EOLproc(ref buffer, lines);
-                return false;
-        }
-        _currentModus = PreLexModus.Default;
-        return true;
-    }
-
-    private bool ToStringModus(int c)
-    {
-        if (c == '"')
-            return true;
-
-        return false;
-    }
-
-    private bool ToCommentModus(int c)
-    {
-        if (c == _settings.CommentStarter)
-            return true;
-
-        return false;
-    }
-
-    #endregion
 
     public string[] Test()
     {
         using FileStream stream = _file.OpenRead();
+        CsvLiner liner = new(stream, _settings);
         int c;
         Console.WriteLine("Binary Value");
         while ((c = stream.ReadByte()) != -1)
@@ -165,6 +43,6 @@ public class CsvLexer
         Console.WriteLine("\n----\n");
 
         stream.Position = 0;
-        return GetLines(stream);
+        return liner.GetLines();
     }
 }
--- Lexer/Csv/CsvLiner.cs
+++ Lexer/Csv/CsvLiner.cs
@@ -1,6 +1,132 @@
-﻿namespace Lexer.Csv;
+﻿using Lexer.Csv.Modus;
 
-public class CsvLiner
+namespace Lexer.Csv;
+
+internal class CsvLiner
 {
+    private int _lastChar = 0;
+    private string _lineBuffer = string.Empty;
+    private readonly List<string> _lines = new();
+    private PreLexModus _currentModus = PreLexModus.Default;
+    private CsvSettings _settings;
+
+    private Stream _stream;
+
+    private Dictionary<PreLexModus, Func<bool>> _modi = new();
+
+    internal CsvLiner(Stream stream, CsvSettings settings)
+    {
+        _stream = stream;
+        _settings = settings;
+        ModiPopulator();
+    }
+
+    private void ModiPopulator()
+    {
+        _modi.Clear();
+
+        _modi.Add(PreLexModus.Default, DefaultHandler);
+        _modi.Add(PreLexModus.String, StringHandler);
+        _modi.Add(PreLexModus.Comment, CommentHandler);
+    }
+
+    internal string[] GetLines()
+    {
+        while (_lastChar != -1)
+        {
+            _modi[_currentModus]();
+        }
+
+        return _lines.ToArray();
+    }
+
+    private bool _EOLProc()
+    {
+        string trimmed = _lineBuffer.Trim();
+        if (_settings.Patches && trimmed.Length == 0)
+            return false;
+        
+        _lines.Add(trimmed);
+        _lineBuffer = "";
+        return true;
+    }
     
+    private bool DefaultHandler()
+    {
+        while ((_lastChar = _stream.ReadByte()) != -1)
+        {
+            if (ToCommentModus(_lastChar))
+            {
+                _currentModus = PreLexModus.Comment;
+                return true;
+            }
+
+            if (ToStringModus(_lastChar))
+            {
+                _currentModus = PreLexModus.String;
+                return true;
+            }
+
+
+            if (_lastChar == '\n')
+            {
+                _EOLProc();
+                continue;
+            }
+
+            _lineBuffer += (char)_lastChar;
+        }
+
+        _EOLProc();
+        
+        return false;
+    }
+
+    private bool StringHandler()
+    {
+        string dbqBuffer = string.Empty;
+        _lastChar = _stream.ReadByte();
+        if (_lastChar == '"')
+            throw new Exception("Cannot start string with two double-quotes \"\"");
+
+        _lineBuffer += (char)_lastChar;
+        while ((_lastChar = _stream.ReadByte()) != -1)
+        {
+            if (_lastChar == '"')
+            {
+                
+            }
+        }
+
+        throw new NotImplementedException();
+    }
+
+    private bool CommentHandler()
+    {
+        // keep consuming till the end of the line
+        while ((_lastChar = _stream.ReadByte()) is not '\n' and not -1) ;
+        switch (_lastChar)
+        {
+            case '\n':
+                _EOLProc();
+                break;
+            
+            case -1:
+                _EOLProc();
+                return false;
+        }
+
+        _currentModus = PreLexModus.Default;
+        return true;
+    }
+
+    private bool ToStringModus(int c) => c == '"';
+    private bool ToCommentModus(int c) => c == _settings.CommentStarter;
+
+    private int SeekByte()
+    {
+        int c = _stream.ReadByte();
+        _stream.Position--;
+        return c;
+    }
 }
